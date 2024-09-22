8ebb300224183d97f4dbb799f5bfbfeb5e5cfb01
air <talko.alexander@gmail.com>
2024-09-22 11:40:18 +0400

    add command superuser

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,7 +5,7 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = User.objects.create(email='admin@admin.com')
+        user = User.objects.create(phone='12223334455')
         user.set_password('123456')
         user.is_active = True
         user.is_staff = True
