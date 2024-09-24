4600085f4d87231a8c1acfa1ab9aa0d94ba5b84f
air <talko.alexander@gmail.com>
2024-09-24 20:26:57 +0400

    add ProfileSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -6,4 +6,16 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'phone', 'first_name', 'last_name', 'invite_code')
+        fields = ('id', 'phone', 'first_name', 'last_name', 'invite_code', 'ref_code',)
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    reference = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ('id', 'phone', 'first_name', 'last_name', 'country', 'invite_code', 'ref_code', 'reference')
+
+    def get_reference(self, obj):
+        user_list = User.objects.filter(ref_code=obj.invite_code)
+        return [user.phone for user in user_list]
