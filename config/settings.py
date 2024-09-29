ea38eb24d1fd297e91a120cacd62826d70b7be48
air <talko.alexander@gmail.com>
2024-09-29 21:14:22 +0400

    add bootstrap

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     # 'phonenumber_field',
 
     'users',
+    'users_interface',
 
 ]
 
@@ -110,6 +111,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (BASE_DIR / 'static',)
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
