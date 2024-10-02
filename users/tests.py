455423ce2579d54461595cedb54390ce511fca83
air <talko.alexander@gmail.com>
2024-10-02 17:13:12 +0400

    add tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from users.models import User
+from users.services import generate_code
+
+
+class UserTest(APITestCase):
+    """Тестирование регистрации пользователя"""
+
+    def setUp(self):
+        self.user = User.objects.create(phone='79991234567', invite_code='03m21C', first_name='Pest')
+
+
+    def test_user_create(self):
+        """Проверяем создание пользователя"""
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(self.user.phone, '79991234567')
+        self.assertEqual(self.user.invite_code, '03m21C')
+        self.assertEqual(self.user.first_name, 'Pest')
+
+    def test_user_update(self):
+        """Проверяем обновление полей пользователя"""
+        self.user.first_name = 'Test'
+        self.user.last_name = 'Pestov'
+        self.user.save()
+        self.assertEqual(self.user.first_name, 'Test')
+        self.assertEqual(self.user.last_name, 'Pestov')
+
+
