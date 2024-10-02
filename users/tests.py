aae520a068923cd367b2fe822c02efe6bd3134ff
air <talko.alexander@gmail.com>
2024-10-02 20:36:16 +0400

    upd tests.py

--- users/tests.py
+++ users/tests.py
@@ -9,16 +9,17 @@ class UserTest(APITestCase):
 
     def setUp(self):
         self.user = User.objects.create(phone='79991234567', invite_code='03m21C', first_name='Pest', country='United')
-        # self.user = User.objects.create(phone='79991234568', invite_code='03m21R', ref_code='03m21C')
+        self.user1 = User.objects.create(phone='79991234568', invite_code='03m21R', ref_code='03m21C')
 
     def test_user_create(self):
         """Проверяем создание пользователя"""
-        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.count(), 2)
         self.assertEqual(self.user.phone, '79991234567')
+        self.assertEqual(self.user1.phone, '79991234568')
         self.assertEqual(self.user.invite_code, '03m21C')
         self.assertEqual(self.user.first_name, 'Pest')
         self.assertEqual(self.user.country, 'United')
-        # self.assertEqual(self.user.invite_code, '03m21C')
+        self.assertEqual(self.user1.invite_code, '03m21R')
 
     def test_user_update(self):
         """Проверяем обновление полей пользователя"""
@@ -29,7 +30,7 @@ class UserTest(APITestCase):
         self.user.save()
         self.assertEqual(self.user.first_name, 'Test')
         self.assertEqual(self.user.last_name, 'Pestov')
-        self.assertEqual(self.user.get_reference(), ['79991234567'])
+        self.assertEqual(self.user.get_reference(), ['79991234568', '79991234567'])
         self.assertEqual(self.user.ref_code, '03m21C')
         self.assertEqual(self.user.country, 'US')
 
