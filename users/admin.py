9e907b9484032a06208523fb4de57ee17679c57b
air <talko.alexander@gmail.com>
2024-10-02 16:33:26 +0400

    upd migrations

--- users/admin.py
+++ users/admin.py
@@ -5,6 +5,6 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'phone', 'first_name', 'last_name',)
-    search_fields = ('phone', 'first_name', 'last_name',)
-    list_filter = ('id', 'phone', 'country',)
+    list_display = ('id', 'phone', 'first_name', 'last_name', 'ref_code',)
+    search_fields = ('phone', 'first_name', 'last_name', 'ref_code',)
+    list_filter = ('id', 'phone', 'country','ref_code',)
