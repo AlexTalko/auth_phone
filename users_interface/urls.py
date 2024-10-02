bea2ef467ddc709b2b8d76d42fdb58fdc3d22823
air <talko.alexander@gmail.com>
2024-10-02 16:34:09 +0400

    upd urls.py

--- users_interface/urls.py
+++ users_interface/urls.py
@@ -1,11 +1,14 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 
-from users_interface.views import login
+from users_interface.views import UserCreateView, SmsCodeView, ProfileView
 from users_interface.apps import UsersInterfaceConfig
 
 app_name = UsersInterfaceConfig.name
 
 urlpatterns = [
-    path('', login, name='login'),
+    path('', UserCreateView.as_view(), name='login'),
+    path('sms_code', SmsCodeView.as_view(), name='sms_code'),
+    path('profile', ProfileView.as_view(), name='profile'),
 ]
