e73ca8d547adf452ab49ce036ec20f14b283242b
air <talko.alexander@gmail.com>
2024-10-03 16:49:40 +0400

    edit clean_ref_code (добавлена проверка на ref_code

--- users_interface/forms.py
+++ users_interface/forms.py
@@ -18,15 +18,15 @@ class UserUpdateForm(UserChangeForm):
 
     def clean_ref_code(self):
         """Проверяем ref_code"""
-
         ref_code = self.cleaned_data.get('ref_code')
-        if ref_code:
-            if ref_code == self.instance.invite_code:
-                raise forms.ValidationError('Вы не можете использовать свой же код!')
-            if not User.objects.filter(invite_code=ref_code).exists():
-                raise forms.ValidationError('Пригласительный код не найден!')
-            # if User.objects.filter(ref_code=ref_code).exists():
-            #     raise forms.ValidationError('Уже использован код')
+        if self.instance.ref_code:
+            raise forms.ValidationError('Вы уже использовали код')
+        if not ref_code:
+            return ref_code
+        if ref_code == self.instance.invite_code:
+            raise forms.ValidationError('Вы не можете использовать свой же код!')
+        if not User.objects.filter(invite_code=ref_code).exists():
+            raise forms.ValidationError('Пригласительный код не найден!')
         return ref_code
 
     class Meta:
